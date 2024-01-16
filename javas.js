014458d249d236cd04c1927cfdb4fe25a894170f
zarqa <zarqakhanum81@gmail.com>
2024-01-16 22:35:46 +0700

    javas

--- javas.js
+++ javas.js
@@ -1,3 +1,5 @@
 function calculator(){
     const number1 = parseFloat(document.getElementsByName("input1").value);
+    const number2 = number1 + number2;
+    document.getElementsByName(h5).innerhtml
 }
