ecf0b96cb703c8571eb4a4b86439290224e083d8
zarqa <zarqakhanum81@gmail.com>
2024-01-18 22:58:07 +0700

    javas

--- javas.js
+++ javas.js
@@ -2,5 +2,11 @@ function calculator(){
     const number1 = parseFloat(document.getElementsByName("input1")[0].value);
     const number2 = parseFloat(document.getElementsByName("input2")[0].value);
     const result = number1 + number2;
+    const high5 = document.getElementsByName("h5")[0];
     document.getElementsByName("h5")[0].innerText = result
+if(!isNaN(result)){
+    h5tag.innerText = result;
+}else{
+    h5tag.innertext ="bitch put number its is calculater not your moms chat";
+}
 }
